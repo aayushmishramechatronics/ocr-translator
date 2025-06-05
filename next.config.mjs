d9ddebc671e4dd32d29fa95254814b9b5cad9db1
Aayush Mishra <manofganges@gmail.com>
2025-06-05 09:59:33 +0530

    Add files via upload

---
+++ next.config.mjs
@@ -0,0 +1,28 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  experimental: {
+    serverActions: {
+      allowedOrigins: ['localhost:3000', '*.vercel.app']
+    }
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  images: {
+    domains: ['blob.v0.dev'],
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'blob.v0.dev',
+        port: '',
+        pathname: '/**',
+      },
+    ],
+    unoptimized: true,
+  },
+}
+
+export default nextConfig
